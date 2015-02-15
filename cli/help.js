0c491a14c9e88883e72085fbda17175f22987076
Chris Badahdah <a.s.badahdah@gmail.com>
2015-02-15 01:23:11 -0700

    Merge changes.

--- cli/help.js
+++ cli/help.js
@@ -1,10 +1,12 @@
 var log = require('../lib/log');
+
 var path = require('path');
 var bin = path.basename(process.argv[1]);
 
 exports.description = "Print help for specific command";
 
 exports.run = function (args) {
+
   try {
     var cmd = require('./' + args._[1]);
 
@@ -16,6 +18,7 @@ exports.run = function (args) {
     }
 
   } catch (e) {
+    log.bug(e);
     log.err('Command not found: ' + args._[1]);
     process.exit(1);
   }
