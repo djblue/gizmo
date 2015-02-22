041ea93d75dc861945c55359bcf182cebe8631a2
Chris Badahdah <a.s.badahdah@gmail.com>
2015-02-22 12:46:09 -0700

    Updated help to complete commands.

--- cli/help.js
+++ cli/help.js
@@ -1,9 +1,30 @@
 var log = require('../lib/log');
 
+var fs = require('fs');
 var path = require('path');
 var bin = path.basename(process.argv[1]);
 
 exports.description = "Print help for specific command";
+
+exports.usage = function () {
+};
+
+exports.completion = function (tabtab, data) {
+  if (data.words < 3) {
+    var cmds = fs.readdirSync(__dirname)
+      .filter(function (file) {
+        // only include js files and not this index file
+        return file.match(/\.js$/)
+           && !file.match('index')
+           && !file.match('help');
+      })
+      .map(function (file) {
+        // remove file extensions
+        return file.replace('.js', '');
+      });
+    return tabtab.log(cmds, data);
+  }
+};
 
 exports.run = function (args) {
 
