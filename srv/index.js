be74b85a4956181a4b81e62a0e6fe091865e7991
Chris Badahdah <a.s.badahdah@gmail.com>
2015-02-23 00:14:46 -0700

    Started using cors module.

--- srv/index.js
+++ srv/index.js
@@ -9,12 +9,8 @@ var compression = require('compression');
 app.use(compression());
 app.use(morgan('dev'));
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// enable cors on all routes
+app.use(require('cors')());
 
 var bodyParser = require('body-parser');
 
